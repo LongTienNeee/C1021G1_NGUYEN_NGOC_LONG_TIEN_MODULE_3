a54904e0d38450cd24b8a10f256ca280911fc86f
LongTien <h2so4allii@gmail.com>
2022-01-14 14:33:38 +0700

    update

---
+++ 3. Thao tác với CSDL/Bài Tập/[Bài tập] Thao tác với CSDL Quản lý bán hàng/thao_tac_csdl_quan_li_ban_hang.sql
@@ -0,0 +1,15 @@
+use quan_li_ban_hang;
+
+insert into customer (customer_name , customer_age)
+values ('Minh Quan', 10),('Ngoc Oanh',20), ('Hong Ha', 50);
+
+insert into `order`
+values (1,1,"2006/03/21/ 00:00:00",null), (2,2,"2006/03/23 00:00:00",null), (3,1,"2022/03/25 00:00:00",null);
+
+insert into product (product_name, product_price)
+values ('May Giat' , 3),
+('Tu Lanh', 5),
+('Dieu Hoa', 7),
+('Quat', 1),
+('Bep Dien', 2);
+
--- 3. Thao tác với CSDL/Bài Tập/[Bài tập] Truy vấn dữ liệu với CSDL Quản lý sinh viên/truy_van_quan_li_sinh_vien.sql
+++ 3. Thao tác với CSDL/Bài Tập/[Bài tập] Truy vấn dữ liệu với CSDL Quản lý sinh viên/truy_van_quan_li_sinh_vien.sql
@@ -15,7 +15,8 @@ update student set class_id = 2
 where student_name = "hung";
 set SQL_SAFE_UPDATES = 1;
 
-select student.student_id, student.student_name, mark from student
-join mark on student.student_id = mark.student_id
-order by mark desc;
+select student.student_name, `subject`.subject_name, mark.Mark from student student
+join mark mark on student.student_id = mark.student_id
+join `subject` `subject` on mark.subject_id = `subject`.subject_id
+order by mark desc, student_name asc;  
 
